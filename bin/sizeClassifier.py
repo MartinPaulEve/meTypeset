14e54dd4a684349e50883137c09849bfd7578837
Martin Eve <martin@martineve.com>
2013-12-27 18:38:23 +0000

    Fix indents on size classifier

--- bin/sizeClassifier.py
+++ bin/sizeClassifier.py
@@ -30,43 +30,44 @@ class sizeClassifier():
     	    list_to_add_to.append(str_to_add)
 
     def get_values(self, tree, search_attribute):
-	# this function searches the DOM tree for TEI "hi" elements with the specified search_attribute
-	sizes = []
-	for child in tree.xpath("//tei:hi[@" + search_attribute + "=not('')]", namespaces={'tei': 'http://www.tei-c.org/ns/1.0'}):
-		self.addToList(sizes, child.get(search_attribute))
+		# this function searches the DOM tree for TEI "hi" elements with the specified search_attribute
+		sizes = []
+		for child in tree.xpath("//tei:hi[@" + search_attribute + "=not('')]", namespaces={'tei': 'http://www.tei-c.org/ns/1.0'}):
+			self.addToList(sizes, child.get(search_attribute))
 
-        return sizes
+		return sizes
 
     def set_dom_tree(self, filename):
-        p = etree.XMLParser(remove_blank_text=True, resolve_entities=False)
-        return etree.parse(filename, p)
-        
+			p = etree.XMLParser(remove_blank_text=True, resolve_entities=False)
+
+			return etree.parse(filename, p)    
        
     def run(self):
-	# load the DOM
-        tree = self.set_dom_tree(self.gv.TEI_FILE_PATH)
+			# load the DOM
+		  	tree = self.set_dom_tree(self.gv.TEI_FILE_PATH)
 
-	# get a numerical list of explicit size values inside meTypesetSize attributes
-	sizes = self.get_values(tree, "meTypesetSize")
+			# get a numerical list of explicit size values inside meTypesetSize attributes
+			sizes = self.get_values(tree, "meTypesetSize")
 	
-	if self.gv.debug:
-		print "Explicitly specified size variations:"
-		print sizes
+			if self.gv.debug:
+				print "Explicitly specified size variations:"
+				print sizes
 
-	# depending on the length of the array we will feed parse differently
+			# depending on the length of the array we will feed parse differently
 
-	if len(sizes) == 1:
-		if self.gv.debug:
-			print "Found single case heading"
+			if len(sizes) == 1:
+				if self.gv.debug:
+					print "Found single case heading"
 		
-		manipulate = Manipulate(self.gv)
+			if len(sizes) > 0:
+				manipulate = Manipulate(self.gv)
 
-		# instruct the manipulator to change the parent tag of every tag it finds containing 
-		# a "hi" tag with meTypesetSize set to the value found to "title"
-		# so, for example <p><hi meTypesetSize="18">some text</hi></p>
-		# will be transformed to
-		# <title><hi meTypesetSize="18">some text</hi></title>
-		manipulate.change_outer("//tei:hi[@meTypesetSize='" + sizes[0] + "']", "head")
+				# instruct the manipulator to change the parent tag of every tag it finds containing 
+				# a "hi" tag with meTypesetSize set to the value found to "title"
+				# so, for example <p><hi meTypesetSize="18">some text</hi></p>
+				# will be transformed to
+				# <title><hi meTypesetSize="18">some text</hi></title>
+				manipulate.change_outer("//tei:hi[@meTypesetSize='" + sizes[0] + "']", "head")
 	
 
 
