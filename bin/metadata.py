98dcf59dfe09ede306d6a5aa79e47e426d746dab
Martin Eve <martin@martineve.com>
2014-01-20 11:38:28 +0000

    Reformat file to tabs

--- bin/metadata.py
+++ bin/metadata.py
@@ -7,26 +7,27 @@ import subprocess, shutil
 __author__ = "Dulip Withanage"
 __email__ = "dulip.withanage@gmail.com"
 
+
 class Metadata():
-	def __init__(self, gv):
-		self.gv = gv
+    def __init__(self, gv):
+        self.gv = gv
 
 
-	def attach_metadata(self):
-		cmd = ["java", "-classpath", self.gv.JAVA_CLASS_PATH,
-					 "-Dxml.catalog.files=" + self.gv.RUNTIME_CATALOG_PATH,
-					 "net.sf.saxon.Transform",
-					 "-x", "org.apache.xml.resolver.tools.ResolvingXMLReader",
-					 "-y", "org.apache.xml.resolver.tools.ResolvingXMLReader",
-					 "-r", "org.apache.xml.resolver.tools.CatalogResolver",
-					 "-o", self.gv.NLM_FILE_PATH,
-					 self.gv.NLM_TEMP_FILE_PATH,
-					 self.gv.METADATA_STYLE_SHEET_PATH,
-					 'metadataFile='+ self.gv.metadata_file
-					 ]
-		return ' '.join(cmd)
+    def attach_metadata(self):
+        cmd = ["java", "-classpath", self.gv.JAVA_CLASS_PATH,
+               "-Dxml.catalog.files=" + self.gv.RUNTIME_CATALOG_PATH,
+               "net.sf.saxon.Transform",
+               "-x", "org.apache.xml.resolver.tools.ResolvingXMLReader",
+               "-y", "org.apache.xml.resolver.tools.ResolvingXMLReader",
+               "-r", "org.apache.xml.resolver.tools.CatalogResolver",
+               "-o", self.gv.NLM_FILE_PATH,
+               self.gv.NLM_TEMP_FILE_PATH,
+               self.gv.METADATA_STYLE_SHEET_PATH,
+               'metadataFile=' + self.gv.metadata_file
+        ]
+        return ' '.join(cmd)
 
-	def run(self):
-		java_command = self.attach_metadata()
-		subprocess.call(java_command, stdin=None, shell=True)
+    def run(self):
+        java_command = self.attach_metadata()
+        subprocess.call(java_command, stdin=None, shell=True)
 
