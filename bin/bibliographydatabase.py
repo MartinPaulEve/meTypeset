5f177364d8290f47d8216b548e01c4f271336ff2
Martin Eve <martin@martineve.com>
2014-02-04 15:20:42 +0000

    Scan of bibliographic elements in an existing document

--- bin/bibliographydatabase.py
+++ bin/bibliographydatabase.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-from teimanipulate import *
-
 __author__ = "Martin Paul Eve"
 __email__ = "martin@martineve.com"
 
@@ -8,11 +5,60 @@ __email__ = "martin@martineve.com"
 A class that scans for, stores and retrieves NLM citations
 
 """
-
+from CodernityDB.database import Database
 from debug import Debuggable
 import tempfile
 from nlmmanipulate import NlmManipulate
 import os
+
+
+class Person():
+    def __init__(self, firstname='', lastname=''):
+        self.firstname = firstname
+        self.lastname = lastname
+
+    def get_citation(self):
+        return u'<name>' \
+                    u'<surname>{0}</surname>' \
+                    u'<given-names>{1}</given-names>' \
+                u'</name>'.format(self.lastname, self.firstname)
+
+class JournalArticle():
+    def __init__(self, authors=[], title='', journal='', issue='', volume='', fpage='', lpage='', year='', doi=''):
+        self.authors = authors
+        self.title = title
+        self.journal = journal
+        self.issue = issue
+        self.volume = volume
+        self.fpage = fpage
+        self.lpage = lpage
+        self.year = year
+        self.doi = doi
+
+    def get_citation(self):
+
+        author_block = ''
+        for author in self.authors:
+            author_block += author.get_citation()
+
+        return u'<ref>'  \
+                    u'<element-citation publication-type="journal">' \
+                        u'<person-group person-group-type="author">' \
+                            u'{0}' \
+                            u'</person-group>' \
+                            u'<article-title>{1}</article-title>' \
+                            u'<source>{2}</source>' \
+                            u'<date>' \
+                                u'<year>{3}</year>' \
+                            u'</date>' \
+                            u'<volume>{4}</volume>' \
+                            u'<issue>{5}</issue>' \
+                            u'<fpage>{6}</fpage>' \
+                            u'<lpage>{7}</lpage>' \
+                            u'<pub-id pub-id-type="doi">{8}</pub-id>' \
+                        u'</element-citation>' \
+                u'</ref>'.format(author_block, self.title, self.journal, self.year, self.volume, self.issue, self.fpage,
+                                self.lpage, self.doi)
 
 
 class BibliographyDatabase(Debuggable):
@@ -29,11 +75,56 @@ class BibliographyDatabase(Debuggable):
 
         manipulate = NlmManipulate(self.gv)
 
-        tree = manipulate.return_elements('//element-citation')
+        # we /could/ use objectify, which would be cleaner, but it doesn't allow such rigidity of parsing
+
+        # scan for journal items
+        tree = manipulate.return_elements('//element-citation[@publication-type="journal"]')
 
         for item in tree:
-            # todo: if we have an author, a title and a year, store this in the database
-            print item
+            journal_item = JournalArticle()
+
+            for sub_item in item:
+                if sub_item.tag == 'person-group' and 'person-group-type' in sub_item.attrib:
+                    if sub_item.attrib['person-group-type'] == 'author':
+                        authors = []
+                        for author_item in sub_item:
+                            author = Person()
+                            for names in author_item:
+                                if names.tag == 'surname':
+                                    author.lastname = names.text
+                                elif names.tag == 'given-names':
+                                    author.firstname = names.text
+                            authors.append(author)
+                        journal_item.authors += authors
+
+                elif sub_item.tag == 'article-title':
+                    journal_item.title = sub_item.text
+
+                elif sub_item.tag == 'source':
+                    journal_item.journal = sub_item.text
+
+                elif sub_item.tag == 'date':
+                    for date_sub in sub_item:
+                        if date_sub.tag == 'year':
+                            journal_item.year = sub_item.text
+
+                elif sub_item.tag == 'volume':
+                    journal_item.volume = sub_item.text
+
+                elif sub_item.tag == 'issue':
+                    journal_item.issue = sub_item.text
+
+                elif sub_item.tag == 'fpage':
+                    journal_item.fpage = sub_item.text
+
+                elif sub_item.tag == 'lpage':
+                    journal_item.lpage = sub_item.text
+
+                elif sub_item.tag == 'pub-id-type' and 'pub-id-type' in sub_item.attrib:
+                    if sub_item.attrib['pub-id-type'] == 'doi':
+                        journal_item.doi = sub_item.text
+
+            print journal_item.get_citation()
 
     def retrieve(self, author, title, year):
         raise NotImplementedError()
