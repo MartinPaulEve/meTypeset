MartinPaulEve/meTypeset

commit b55b890f202557a8ceee917cc3ddced932bf2bb7

Bugfix: handle last heading that falls after a run of headings below the size cutoff

bin/sizeclassifier.py
@@ -204,9 +204,15 @@ class SizeClassifier(Debuggable):
     def process_subsequent_headings(self, iteration, manipulate, processed_flag, section_ids, section_stack, size,
                                     sizes_ordered):
         if not processed_flag:
+            last = True
+
+            # determine if is last
+            for index in range(iteration + 1, len(sizes_ordered) - 1):
+                if float(sizes_ordered[index]) >= float(self.size_cutoff):
+                    last = False
 
             # ascertain the next size
-            if iteration < (len(sizes_ordered) - 1):
+            if not last:
                 next_size, next_id = self.get_next_size(iteration, sizes_ordered)
 
                 if float(size) == float(next_size):
@@ -317,6 +323,11 @@ class SizeClassifier(Debuggable):
                     else:
                         section_ids[section_stack.index(size)] = iteration
 
+                iteration += 1
+
+            else:
+                self.debug.print_debug(self, u'Ignoring heading {0} because its size ({1}) '
+                                             u'is less than {2}'.format(iteration, size, self.size_cutoff))
                 iteration += 1
 
     def get_sizes(self, tree):

commit c47a0d3c04b7cc9363ae6e48093b74f23129ea6f

Fix for regression in b55b890f202557a8ceee917cc3ddced932bf2bb7

bin/sizeclassifier.py
@@ -80,6 +80,11 @@ class SizeClassifier(Debuggable):
         manipulate.enclose_and_change_self_size(expression, str(root_size), 'p', 'hi')
 
     def handle_misstacked(self, next_size, section_ids, section_stack):
+
+        if float(next_size) > float(self.size_cutoff):
+            # normalize the size
+            next_size = self.size_cutoff
+
         sibling_size = 0
         for x in section_stack:
             if x > next_size:
@@ -329,9 +334,9 @@ class SizeClassifier(Debuggable):
                 self.debug.print_debug(self, u'Ignoring heading {0} because its size ({1}) '
                                              u'is less than {2}'.format(iteration, size, self.size_cutoff))
 
-                # TODO: with this line commented out, we don't crash. With it in, we fail to classify the last header
-                # on occasion
-                #iteration += 1
+                # fix for regression at b55b890f202557a8ceee917cc3ddced932bf2bb7 handled by size normalization in
+                # mismatched size handler
+                iteration += 1
 
     def get_sizes(self, tree):
         sizes = self.get_values(tree, "meTypesetSize")