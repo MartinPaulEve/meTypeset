925c5fbffb28e02a52ff7c4bdd94beb6a9289289
Martin Eve <martin@martineve.com>
2014-02-21 11:09:15 +0000

    Table classifier infrastructure

---
+++ bin/tableclassifier.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+"""tableclassfier: a tool to search for potential table titles and then link in-text entities
+
+Usage:
+   tableclassfier.py <input> [options]
+
+Options:
+    -d, --debug                                     Enable debug output
+    -h, --help                                      Show this screen.
+    -v, --version                                   Show version.
+
+"""
+
+from docopt import docopt
+from bare_globals import GV
+from debug import Debuggable
+from nlmmanipulate import NlmManipulate
+
+
+class TableClassifier(Debuggable):
+    def __init__(self, global_variables):
+        self.gv = global_variables
+        self.debug = self.gv.debug
+        Debuggable.__init__(self, 'Table Classifier')
+
+    def run(self):
+        manipulate = NlmManipulate(self.gv)
+
+        tree = manipulate.load_dom_tree()
+
+        tree.write(self.gv.nlm_file_path)
+        tree.write(self.gv.nlm_temp_file_path)
+
+
+def main():
+    args = docopt(__doc__, version='meTypeset 0.1')
+    bare_gv = GV(args)
+
+    if args['--debug']:
+        bare_gv.debug.enable_debug()
+
+    table_classifier_instance = TableClassifier(bare_gv)
+    table_classifier_instance.run()
+
+
+if __name__ == '__main__':
+    main()
