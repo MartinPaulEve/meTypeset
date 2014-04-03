bf75ee9634829bbe099fa04272a2a2ab5259ff31
Martin Eve <martin@martineve.com>
2014-04-03 09:50:47 +0100

    Switch regex method to named group for readability

--- bin/nlmmanipulate.py
+++ bin/nlmmanipulate.py
@@ -477,11 +477,11 @@ class NlmManipulate(Manipulate):
     def clean_refs(self):
         tree = self.load_dom_tree()
 
-        ref_regex = re.compile('^(?P<prelim>\s*\d+[\.\,]?\s+)(.+)')
+        ref_regex = re.compile('^(?P<prelim>\s*\d+[\.\,]?\s+)(?P<reference>.+)')
 
         for ref in tree.xpath('//back/ref-list/ref'):
             if ref.text:
-                ref.text = ref_regex.sub('\\2', ref.text)
+                ref.text = ref_regex.sub('\\g<reference>', ref.text)
 
         self.save_tree(tree)
 
