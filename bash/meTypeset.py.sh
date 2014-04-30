f52998562e99fe70cab59ecdea7ac5ff1dc90356
Martin Paul Eve <martin@martineve.com>
2014-04-30 15:16:35 +0100

    Bash completion script for reference linker

--- bash/meTypeset.py.sh
+++ bash/meTypeset.py.sh
@@ -1,5 +1,5 @@
 
-_meTypesetpy()
+_meTypeset()
 {
     local cur prev
     cur="${COMP_WORDS[COMP_CWORD]}"
@@ -34,11 +34,49 @@ _meTypesetpy()
 				elif [ $COMP_CWORD -eq 3 ]; then
 					COMPREPLY=( $(compgen -d ${cur}) )
 				elif [ $COMP_CWORD -ge 4 ]; then
-					COMPREPLY=( $(compgen -W '-a --aggression --chain -c --clean -d --debug -i --identifiers -h --help -m --metadata --nogit --noimageprocessing --nolink --purenlm --puretei --prettytei -p --proprietary -s --settings -v --version' -- $cur) )
+					COMPREPLY=( $(compgen -W '-a --aggression --chain -c --clean -d --debug -h --help -i --identifiers --interactive -m --metadata --nogit --noimageprocessing --nolink --purenlm --puretei --prettytei -p --proprietary -s --settings -v --version' -- $cur) )
 				fi
 			;;
 		esac
     fi
 }
 
+_reLinker()
+{
+    local cur prev first proceed
+    first="${COMP_WORDS[1]}"
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+	proceed='FALSE'
+
+    if [ $COMP_CWORD -eq 1 ]; then
+        COMPREPLY=( $( compgen -W ' scan link prune' -- $cur) )
+	else
+		if [ "$first" == "scan" ]; then
+			if [ $COMP_CWORD -eq 2 ]; then
+				COMPREPLY=( $(compgen -f ${cur}) )
+			elif [ $COMP_CWORD -ge 3 ]; then
+				proceed="TRUE"
+			fi
+		elif [ "$first" == "link" ]; then
+			if [ $COMP_CWORD -eq 2 ]; then
+				COMPREPLY=( $(compgen -f ${cur}) )
+			elif [ $COMP_CWORD -ge 5 ]; then
+				proceed="TRUE"
+			fi
+		elif [ "$first" == 'prune' ]; then
+			if [ $COMP_CWORD -eq 2 ]; then
+				COMPREPLY=( $(compgen -f ${cur}) )
+			elif [ $COMP_CWORD -ge 3 ]; then
+				proceed="TRUE"
+			fi
+		fi
+		
+		if [ "$proceed" == "TRUE" ]; then
+			COMPREPLY=( $(compgen -W '-d --debug -h --help --interactive -v --version' -- $cur) )
+		fi
+    fi
+}
+
 complete -F _meTypesetpy meTypeset.py
+complete -F _reLinker referencelinker.py
