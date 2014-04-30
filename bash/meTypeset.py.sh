14ea7d58ac444d0343f6cc93797f12ca57110c35
Martin Paul Eve <martin@martineve.com>
2014-04-30 14:51:39 +0100

    More advanced bash completion

--- bash/meTypeset.py.sh
+++ bash/meTypeset.py.sh
@@ -1,17 +1,43 @@
 
 _meTypesetpy()
 {
-    local cur
+    local cur prev
     cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
 
     if [ $COMP_CWORD -eq 1 ]; then
         COMPREPLY=( $( compgen -W ' tei docx doc odt docxextracted other bibscan' -- $cur) )
-    elif [ $COMP_CWORD -eq 2 ]; then
-        COMPREPLY=( $(compgen -f ${cur}) )
-    elif [ $COMP_CWORD -eq 3 ]; then
-        COMPREPLY=( $(compgen -d ${cur}) )
-	elif [ $COMP_CWORD -ge 3 ]; then
-		COMPREPLY=( $(compgen -W '-a --aggression --chain -c --clean -d --debug -i --identifiers -h --help -m --metadata --nogit --noimageprocessing --nolink --purenlm --puretei --prettytei -p --proprietary -s --settings -v --version' -- $cur) )
+	else
+		case $prev in
+			--agression)
+			;;
+			-a)
+			;;
+			--chain)
+				COMPREPLY=( $(compgen -f ${cur}) )
+			;;
+			--metadata)
+				COMPREPLY=( $(compgen -f ${cur}) )
+			;;
+			-m)
+				COMPREPLY=( $(compgen -f ${cur}) )
+			;;
+			--settings)
+				COMPREPLY=( $(compgen -f ${cur}) )
+			;;
+			-s)
+				COMPREPLY=( $(compgen -f ${cur}) )
+			;;
+			*)
+				if [ $COMP_CWORD -eq 2 ]; then
+					COMPREPLY=( $(compgen -f ${cur}) )
+				elif [ $COMP_CWORD -eq 3 ]; then
+					COMPREPLY=( $(compgen -d ${cur}) )
+				elif [ $COMP_CWORD -ge 4 ]; then
+					COMPREPLY=( $(compgen -W '-a --aggression --chain -c --clean -d --debug -i --identifiers -h --help -m --metadata --nogit --noimageprocessing --nolink --purenlm --puretei --prettytei -p --proprietary -s --settings -v --version' -- $cur) )
+				fi
+			;;
+		esac
     fi
 }
 
